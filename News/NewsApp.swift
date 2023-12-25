7f5178b780d5d6b40d9b855402f4b65d322586b0
Yaroslav <yaroslav.0029@gmail.com>
2023-12-25 16:47:55 +0100

    Project init

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Yaroslav Chlek on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ News/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  News
+//
+//  Created by Yaroslav Chlek on 25.12.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
